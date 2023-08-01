7976321144b191e6906a88554b63ae8ee7674129
LeylaHajiyeva <l.haciyeva19@gmail.com>
2023-08-01 10:54:15 +0400

    new tasks added

--- Main.java
+++ Main.java
@@ -58,11 +58,61 @@ public class Main {
         System.out.println(reverse);
     }
 
+//            4. Verlmiş ədədin palindrome olub-olmamasını yoxlayın. Məs: İnput: 123321 Output: true
+
+//            6.  Verilmiş ədədin rəqəmlərinin unikal(təkrarlanmayan) olub-olmamasını yoxlayın.
+//    Məs: İnput: 53213 Output: false(3 ədədi təkrarlanır)
+
+    //       5. Verilmiş ədədin neçə mərtəbəli olduğunu təyin edin. Məs: İnput: 23456  Output: 5
+    static void myFunc5(int number) {
+        int a = number;
+        int count = 0;
+        do {
+            count++; //4
+            a = a / 10; //5
+
+        } while (a % 10 != 0);
+        System.out.println(count);
+    }
+
+    //            6. Verlmiş ədədin sadə rəqəmlərinin cəmini tapın. Məs: İnput: 123456  Output: 11
+    static void myFunc6() {
+        int number = 5432;
+        int a = number;
+        int sum = 0;
+        int count = 0;
+        while (a % 10 != 0) {
+            for (int i = 1; i <= (a % 10) / 2; i++) {
+                if (((a % 10) % i) == 0) {
+                    System.out.println("a%10 " + (a % 10));
+                    count++;
+                    System.out.println("count " + count);
+                }
+            }
+            a = a / 10;
+        }
+    }
+
+    //            7. İnteger massivində verilmiş ədədlərin ən böyüyünü tapın. Məs: İnput: [10,23,2,45,7]  Output: 45
+    static void myFunc7() {
+        int[] myarr = new int[]{10, 23, 2, 45, 7};
+        int element = myarr[0];
+        for (int i = 0; i < myarr.length; i++) {
+            if (element < myarr[i]) {
+                element = myarr[i];
+            }
+        }
+        System.out.println(element);
+
+    }
 
     public static void main(String[] args) {
-        myFunc1();
-        myFunc2();
-        myFunc3();
-        myFunc4();
+//        myFunc1();
+//        myFunc2();
+//        myFunc3();
+//        myFunc4();
+//        myFunc5(54321);
+//        myFunc6();
+        myFunc7();
     }
 }
